15e733df0b06176e1005c2066140c9f3335d9a65
hongnhat1121 <1951052145nhat@ou.edu.vn>
2022-03-18 00:26:55 +0700

    update code 12:26am 18/3/2022

--- E-Commerce/saleMobile/saleapp/admin.py
+++ E-Commerce/saleMobile/saleapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+from .models import Category, Product, User, Customer
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "image", "price", "manufacturer", "description", "active",
+                    "category"]
+    list_filter = ["category", "manufacturer"]
+    search_fields = ["name", "price", "description", "category__name"]
+    readonly_fields = ["product_image"]
+
+    def product_image(self, product):
+        return mark_safe("<img src='/static/{img_url}' alt='{alt}' width='120px'/>".format(img_url=product.image.name, alt=product.name))
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(User)
+admin.site.register(Customer)
--- E-Commerce/saleMobile/saleapp/models.py
+++ E-Commerce/saleMobile/saleapp/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+    avatar = models.ImageField(upload_to='Avatars/%Y/%m')
 
 
 class Category(models.Model):
+    class Meta:
+        ordering = ['id']
+
     name = models.CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Customer(models.Model):
@@ -17,13 +24,25 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
+    class Meta:
+        ordering = ['id']
+
     name = models.CharField(max_length=50, null=False, unique=True)
-    image = models.ImageField(upload_to='uploads/%Y/%m')
-    price = models.DecimalField(decimal_places=3, max_digits=10)
+    image = models.ImageField(upload_to='Products/%Y/%m')
+    price = models.DecimalField(decimal_places=3, max_digits=12)
     manufacturer = models.CharField(max_length=50, null=False)
     description = models.TextField(null=True, blank=True)
     active = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, related_name="category", on_delete=models.SET_NULL,
+                                 null=True)
+
+
+class Specification(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    monitor = models.TextField(max_length=255, null=True, blank=True)
+    processors = models.TextField(max_length=255, null=True, blank=True)
+    RAM = models.IntegerField()
+    battery = models.IntegerField()
 
 
 class StatusOrder(models.Model):
