76e02863e5bfba60358fa59ca88de9c51f28b50c
tandat0723 <trantandat0723@gmail.com>
2022-03-19 22:20:15 +0700

    search and stats

--- E-Commerce/saleMobile/saleMobile/urls.py
+++ E-Commerce/saleMobile/saleMobile/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from saleapp.admin import admin_site
 
 urlpatterns = [
     path('', include('saleapp.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
